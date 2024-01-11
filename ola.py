cbd5563756355fe9abe2e963082562282e287537
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2024-01-11 09:28:45 -0300

    Update ola.py

--- ola.py
+++ ola.py
@@ -1,8 +1,18 @@
-print("oi")
-print("QUero testar mudanças")
-
-    
 import random
+ja_sorteados = [11]
 
-sorteio = random.randrange(1,10)
-print(sorteio)  
+def sorteia():
+    while True:
+        sorteio = random.randrange(1,11)
+        if sorteio not in ja_sorteados:
+            print(sorteio)
+            ja_sorteados.append(sorteio)
+            
+
+        else:
+            continue
+        
+    if ja_sorteados.lenght() == 11:
+        return
+        
+sorteia()
